6f15567a2bce167363c74840e5fc18c899226b36
Water.Li <lizongbin@hotmail.com>
2021-09-02 14:36:11 +0800

    add magic upload

---
+++ src/hooks/useLazyMagicUpload.ts
@@ -0,0 +1,24 @@
+import { AxiosRequestConfig } from "axios";
+import { API_MAGIC_UPLOAD } from "../helper/api";
+import { DataError } from "../helper/DataError";
+import { useLazyAxios } from "./useLazyAxios";
+
+export function useLazyMagicUpload<T>(
+    options?:{
+      onCompleted?:(data:T)=>void,
+      onError?:(error:any)=>void,
+    }      
+  )
+  :[(config?:AxiosRequestConfig)=>void, {loading?:boolean, data?:T, error?:DataError}] 
+{
+  const rtValue = useLazyAxios({
+    ...API_MAGIC_UPLOAD, 
+    headers:{
+      ...API_MAGIC_UPLOAD.headers || {},
+      "Content-Type": "multipart/form-data;boundary=" + new Date().getTime()
+    }
+  }, 
+  options
+);
+  return rtValue;
+}
